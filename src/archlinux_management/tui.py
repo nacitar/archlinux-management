9cef3afa80f3047aafaf6dbe9c19e2e213489653
Jacob McIntosh <nacitar.sevaht@gmail.com>
2025-03-27 15:06:49 -0600

    Menu implementation

--- src/archlinux_management/tui.py
+++ src/archlinux_management/tui.py
@@ -1,32 +1,34 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 from .term_style import TermStyle
 
 
-def info(message: str) -> None:
+def info(message: str, *, indent: int = 0, indicator: str = "==>") -> None:
     print(
-        f"{TermStyle.GREEN}{TermStyle.BOLD}==>"
+        f"{' '*indent}{TermStyle.GREEN}{TermStyle.BOLD}{indicator}"
         f"{TermStyle.RESET_COLOR} {message}{TermStyle.RESET}"
     )
 
 
-def detail(message: str) -> None:
+def detail(message: str, *, indent: int = 0, indicator: str = "->") -> None:
     print(
-        f"  {TermStyle.BLUE}{TermStyle.BOLD}->"
+        f"{' '*(indent+2)}{TermStyle.BLUE}{TermStyle.BOLD}{indicator}"
         f"{TermStyle.RESET_COLOR} {message}{TermStyle.RESET}"
     )
 
 
-def warning(message: str) -> None:
+def warning(message: str, *, indent: int = 0, indicator: str = "==>") -> None:
     print(
-        f"{TermStyle.YELLOW}{TermStyle.BOLD}==> WARNING:"
+        f"{' '*indent}{TermStyle.YELLOW}{TermStyle.BOLD}{indicator} WARNING:"
         f"{TermStyle.RESET_COLOR} {message}{TermStyle.RESET}"
     )
 
 
-def error(message: str) -> None:
+def error(message: str, *, indent: int = 0, indicator: str = "==>") -> None:
     print(
-        f"{TermStyle.RED}{TermStyle.BOLD}==> ERROR:"
+        f"{' '*indent}{TermStyle.RED}{TermStyle.BOLD}{indicator} ERROR:"
         f"{TermStyle.RESET_COLOR} {message}{TermStyle.RESET}"
     )
 
@@ -36,8 +38,11 @@ def prompt(
     answers: list[str] | None = None,
     default: str = "",
     *,
+    indent: int = 0,
     lower: bool = True,
     alternates: dict[str, str] | None = None,
+    show_options: bool = True,
+    indicator: str = "::",
 ) -> str:
     if answers:
         answer_display = f" [{
@@ -52,12 +57,13 @@ def prompt(
             )
     else:
         answer_display = ""
-    message = f"{message}{answer_display} "
+    if show_options:
+        message = f"{message}{answer_display}"
     while True:
         answer = (
             input(
-                f"{TermStyle.GREEN}{TermStyle.BOLD}::"
-                f"{TermStyle.RESET_COLOR} {message}{TermStyle.RESET}"
+                f"{' '*indent}{TermStyle.GREEN}{TermStyle.BOLD}{indicator}"
+                f"{TermStyle.RESET_COLOR} {message}{TermStyle.RESET} "
             )
             or default
         )
@@ -67,17 +73,62 @@ def prompt(
             answer = alternates.get(answer, answer)
         if not answers or answer in answers:
             return answer
-        detail("Invalid answer.")
+        detail("Invalid answer.", indent=indent + 2)
     return answer
 
 
 PROMPT_YES_NO_ALTERNATES: dict[str, str] = {"yes": "y", "no": "n"}
 
 
-def prompt_yes_no(message: str, default: str = "") -> bool:
+def prompt_yes_no(
+    message: str, default: str = "", *, show_options: bool = True
+) -> bool:
     return (
         prompt(
-            message, ["y", "n"], default, alternates=PROMPT_YES_NO_ALTERNATES
+            message,
+            ["y", "n"],
+            default,
+            alternates=PROMPT_YES_NO_ALTERNATES,
+            show_options=show_options,
         )
         == "y"
     )
+
+
+@dataclass
+class Menu:
+    class _PreviousMenuError(RuntimeError):
+        pass
+
+    message: str
+    options: dict[str, str | Menu]
+
+    def __prompt(self, allow_back: bool = False) -> str:
+        while True:
+            keys = list(self.options.keys())
+            if not keys:
+                raise ValueError(f"no options provided for {type(self)}.")
+            info(self.message)
+            option_width = len(str(len(keys)))
+            answers = ["0"] if allow_back else []
+            for i in range(len(keys)):
+                print(f"  {i+1:>{option_width}}. {keys[i]}")
+                answers.append(str(i + 1))
+            answer = prompt(
+                f"Choice{' (0 for previous menu)' if allow_back else ''}:",
+                answers,
+                show_options=False,
+            )
+            print()
+            if answer == "0":
+                raise type(self)._PreviousMenuError()
+            value: str | Menu = self.options[keys[int(answer) - 1]]
+            if isinstance(value, Menu):
+                try:
+                    value = value.__prompt(True)
+                except type(self)._PreviousMenuError:
+                    continue
+            return value
+
+    def prompt(self) -> str:
+        return self.__prompt(allow_back=False)
