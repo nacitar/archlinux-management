7992df936463d6e9e4777875eaa052d0da4645cb
Jacob McIntosh <nacitar.sevaht@gmail.com>
2025-03-31 23:45:42 -0600

    Added more modifications

--- src/archlinux_management/modifications.py
+++ src/archlinux_management/modifications.py
@@ -55,6 +55,47 @@ def pc_speaker_device_owned_by_audio_group(
             return updater.remove()
 
 
+def systemd_networkd_wait_for_any_interface_5s_timeout(
+    options: ModificationOptions,
+) -> bool:
+    with FileUpdater.from_resource(
+        target=Path(
+            "/etc/systemd/system/"
+            "systemd-networkd-wait-online.service.d/wait-for-any.conf"
+        ),
+        resource="systemd_networkd_wait_for_any_interface_5s_timeout.conf",
+        options=options,
+    ) as updater:
+        if options.apply:
+            return updater.apply()
+        else:
+            return updater.remove()
+
+
+def xorg_conf_tear_free(options: ModificationOptions) -> bool:
+    with FileUpdater.from_resource(
+        target=Path("/etc/X11/xorg.conf.d/20-tearfree.conf"),
+        resource="xorg_tearfree.conf",
+        options=options,
+    ) as updater:
+        if options.apply:
+            return updater.apply()
+        else:
+            return updater.remove()
+
+
+def sysctl_steamos_vm_max_map_count(options: ModificationOptions) -> bool:
+    with FileUpdater.from_resource(
+        target=Path("/etc/sysctl.d/80-gamecompatibility.conf"),
+        resource="sysctl_steamos_vm.max_map_count.conf",
+        options=options,
+    ) as updater:
+        if options.apply:
+            return updater.apply()
+        else:
+            return updater.remove()
+
+
 MODIFICATION_MENU = Menu[Callable[[ModificationOptions], bool]](
     "Select modification",
     {
@@ -63,6 +104,11 @@ MODIFICATION_MENU = Menu[Callable[[ModificationOptions], bool]](
         "pc speaker device owned by audio group": (
             pc_speaker_device_owned_by_audio_group
         ),
+        "systemd-networkd wait for ANY interface (5s timeout)": (
+            systemd_networkd_wait_for_any_interface_5s_timeout
+        ),
+        "xorg.conf tear free": xorg_conf_tear_free,
+        "SteamOS value for vm.max_map_count": sysctl_steamos_vm_max_map_count,
     },
 )
 MODIFICATION_LOOKUP: dict[str, Callable[[ModificationOptions], bool]] = {
